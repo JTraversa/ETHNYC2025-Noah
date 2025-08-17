14a30e3da759a1c13b246040d3ab1e9e5363dbe9
Julian Traversa <unitedterrorhots@gmail.com>
2025-08-17 00:44:41 -0600

    chiliz

--- src/web3/wagmi.js
+++ src/web3/wagmi.js
@@ -1,12 +1,28 @@
 import { createConfig, http } from 'wagmi';
 import { mainnet, sepolia, arbitrum } from 'wagmi/chains';
 
+// Custom Chiliz chain (id: 88888)
+const chiliz = {
+  id: 88888,
+  name: 'Chiliz',
+  nativeCurrency: { name: 'Chiliz', symbol: 'CHZ', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc.chiliz.com'] },
+    public: { http: ['https://rpc.chiliz.com'] },
+  },
+  blockExplorers: {
+    default: { name: 'ChilizScan', url: 'https://scan.chiliz.com' },
+  },
+  testnet: false,
+};
+
 export const wagmiConfig = createConfig({
-  chains: [mainnet, sepolia, arbitrum],
+  chains: [mainnet, sepolia, arbitrum, chiliz],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
     [arbitrum.id]: http(),
+    [chiliz.id]: http('https://rpc.chiliz.com'),
   },
 });
 
